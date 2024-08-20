a3a8151922b5db475582b0baa5a824c92b4a3111
GlukKluk <glukkluk.dev@gmail.com>
2024-08-20 16:45:02 +0300

    Added a configuration file. Logging settings have been reworked and a basic color log has been added for better readability. Updated requirements.txt and .env_example files.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 
+import betterlogging as bl
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.default import DefaultBotProperties
@@ -9,17 +10,45 @@ from aiogram.enums import ParseMode
 from aiogram_dialog import setup_dialogs
 from environs import Env
 
+from tgbot.config import load_config
 from tgbot.handlers.user import router as user_router
 from tgbot.dialogs import dialogs_list
 
 
-env = Env()
+config = load_config(".env")
 
+
+def setup_logging():
+    """
+    Set up logging configuration for the application.
+
+    This method initializes the logging configuration for the application.
+    It sets the log level to INFO and configures a basic colorized log for
+    output. The log format includes the filename, line number, log level,
+    timestamp, logger name, and log message.
+
+    Returns:
+        None
+
+    Example usage:
+        setup_logging()
+    """
+    log_level = logging.INFO
+    bl.basic_colorized_config(level=log_level)
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s",
+    )
+    logger = logging.getLogger(__name__)
+    logger.info("Starting bot")
 
 
 async def main() -> None:
+    setup_logging()
+    
     bot: Bot = Bot(
-        token=env.str("BOT_TOKEN"),
+        token=config.tg_bot.token,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
         )
     dp: Dispatcher = Dispatcher(storage=MemoryStorage())
@@ -35,5 +64,7 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logging.error("Бот був вимкнений!")
