a5322c54c5de5323d4a1ff9333bb496225f05d66
GlukKluk <glukkluk.dev@gmail.com>
2024-08-24 17:05:35 +0300

    Added a class to interact with the weather API.

---
+++ infrastructure/api/weatherapi.py
@@ -0,0 +1,54 @@
+from typing import Optional
+
+from aiohttp import ClientSession, ClientResponse
+
+from tgbot.config import load_config
+
+
+config = load_config(".env")
+
+class WeatherApiSession:
+    def __init__(self, url: str = config.api.url, api_key: str = config.api.api_key, method: str = config.api.method):
+        self.url = url
+        self.api_key = api_key
+        self.method = method
+        self.session: Optional[ClientSession] = None
+        
+    async def create_session(self) -> ClientSession:
+        if self.session is None or self.session.closed:
+            self.session = ClientSession()
+        
+        return self.session
+    
+    async def close(self) -> None:
+        if self.session is not None and not self.session.closed:
+            await self.session.close()
+    
+    
+    async def check_response(self, response: ClientResponse):
+        match response.status:
+            case 200:
+                return await response.json()
+            
+            case 400:
+                return "Невірно вказано місто"
+            
+            case 403:
+                return "Закінчився ліміт API."
+    
+    async def make_request(self, residence: str):
+        session = await self.create_session()
+        
+        try:
+            async with session.get(
+                url=f"{config.api.url}{config.api.method}",
+                params=dict(key=config.api.api_key, q=residence, days=3, lang="uk")
+            ) as resp:
+                
+                result = await self.check_response(response=resp)
+            
+                return result
+                
+        except Exception as e:
+            print(f"Виникла помилка: {e}")
+            
